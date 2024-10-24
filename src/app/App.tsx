5659cf1f94af586622e889d461e2420dbb3c9f9c
Filipe Silva <filipe_silva@zenbyzenith.com>
2024-10-24 21:18:08 +0000

    feat: visual, folder structure and small component changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,36 +1,34 @@
-import { useState } from 'react'
+import styled from "styled-components";
 
-import viteLogo from '../assets/vite.svg'
-import reactLogo from '../assets/react.svg'
-import './App.css'
+import "./App.css";
+import { reactLogo } from "./assets";
 
 function App(): JSX.Element {
-  const [count, setCount] = useState(0)
+  const Header = styled.div`
+    display: flex;
+    flex-direction: row;
+    height: 20vh;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  const Footer = styled.footer`
+    display: flex;
+    flex-direction: row;
+    height: 10vh;
+    align-items: center;
+    justify-content: center;
+  `;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header>
+        <h1>React Weather App</h1>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </Header>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App;
