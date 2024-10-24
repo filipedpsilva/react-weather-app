22655464da8501962c5fb0713e496736b9649b6d
Filipe Silva <filipe_silva@zenbyzenith.com>
2024-10-24 21:18:50 +0000

    fix: missing constants

---
+++ src/app/data/constants.ts
@@ -0,0 +1,9 @@
+// META
+export const ENV = import.meta.env;
+
+// URLS
+export const BASE_API_URL = ENV.VITE_BASE_API_URL;
+
+// KEYS
+export const API_KEY = ENV.VITE_API_KEY;
+export const ACCESS_KEY = ENV.VITE_ACCESS_KEY;
